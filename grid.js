e8f24b7d0d6e35549852e02a861dfadcc2e509e2
matrecode <akshaymatrecode@gmail.com>
2024-01-14 16:42:11 +0530

    Tried with 52 columns

--- grid.js
+++ grid.js
@@ -1,5 +1,5 @@
 let rows = 100;
-let cols = 26;
+let cols = 26
 
 let addressColContainer = document.querySelector(".address-column-container");
 let addressRowContainer = document.querySelector(".address-row-container");
@@ -20,36 +20,40 @@ for (i = 0; i < rows; i++) {
 for (i = 0; i < cols; i++) {
   let addressRow = document.createElement("div");
   addressRow.setAttribute("class", "address-row");
-
-  addressRow.innerText = String.fromCharCode(65 + i);
+  // if (i < 26) {
+    addressRow.innerText = String.fromCharCode(65 + i); 
+  //}
+  // else {
+  //   addressRow.innerText = "A" + String.fromCharCode(65 + (i - 26))
+  // }
   addressRowContainer.appendChild(addressRow);
 }
 
 
-for(i = 0; i < rows; i++){
-    let cellContainer = document.createElement("div");
-    cellContainer.setAttribute("class", "cell-container");
+for (i = 0; i < rows; i++) {
+  let cellContainer = document.createElement("div");
+  cellContainer.setAttribute("class", "cell-container");
 
-    for(j = 0; j < cols;j++){
-        let cell = document.createElement("div");
-        cell.setAttribute("class", "cell");
-        cell.setAttribute("contentEditable", "true")
-      // Attributes for cell and storage identification
-        cell.setAttribute("rid", i);
-        cell.setAttribute("cid",j)
-        cellContainer.appendChild(cell);
-        addListenerForAddressBarDisplay(cell, i, j);
-    }
+  for (j = 0; j < cols; j++) {
+    let cell = document.createElement("div");
+    cell.setAttribute("class", "cell");
+    cell.setAttribute("contentEditable", "true")
+    // Attributes for cell and storage identification
+    cell.setAttribute("rid", i);
+    cell.setAttribute("cid", j)
+    cellContainer.appendChild(cell);
+    addListenerForAddressBarDisplay(cell, i, j);
+  }
 
-    cellsContainer.appendChild(cellContainer)
+  cellsContainer.appendChild(cellContainer)
 }
 
-function addListenerForAddressBarDisplay(cell, i, j){
-    cell.addEventListener("click", (e)=>{
-        let rowID = i + 1;
-        let colID = String.fromCharCode(65 + j);
-        addressBar.value = `${colID}${rowID}`;
-    })
+function addListenerForAddressBarDisplay(cell, i, j) {
+  cell.addEventListener("click", (e) => {
+    let rowID = i + 1;
+    let colID = String.fromCharCode(65 + j);
+    addressBar.value = `${colID}${rowID}`;
+  })
 }
 
 // by default focus on cell
